533eb4f26fd2bcbc974bd9d908511755259e01d8
Akshit <akpahade55@gmail.com>
2025-03-19 13:01:13 +0530

    Add Hive integration for user data management and refactor authentication flow

--- lib/presentation/components/bottom_navigation.dart
+++ lib/presentation/components/bottom_navigation.dart
@@ -2,7 +2,7 @@ import 'package:animated_bottom_navigation_bar/animated_bottom_navigation_bar.da
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
-import '../screens/navigation_screen.dart';
+import '../../core/controller/Navigation/navigation_controller.dart';
 
 class CustomBottomNavigation extends StatelessWidget {
   final NavigationController controller;
